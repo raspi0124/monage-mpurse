210a967fc7820c39df757327e295b7d14beec0d5
raspi0124 <adores0124@yahoo.co.jp>
2019-07-31 17:24:47 +0900

    Changed base64 encoding script

--- base64.js
+++ base64.js
@@ -1,60 +1,39 @@
-//Code by @weal
-//https://qiita.com/weal/items/1a2af81138cd8f49937d
-var Base64a = {
-  encode: (function(i, tbl) {
-    for (i = 0, tbl = {
-        64: 61,
-        63: 47,
-        62: 43
-      }; i < 62; i++) {
-      tbl[i] = i < 26 ? i + 65 : (i < 52 ? i + 71 : i - 4);
-    } //A-Za-z0-9+/=
-    return function(arr) {
-      var len, str, buf;
-      if (!arr || !arr.length) {
-        return "";
-      }
-      for (i = 0, len = arr.length, buf = [], str = ""; i < len; i += 3) { //6+2,4+4,2+6
-        str += String.fromCharCode(
-          tbl[arr[i] >>> 2],
-          tbl[(arr[i] & 3) << 4 | arr[i + 1] >>> 4],
-          tbl[i + 1 < len ? (arr[i + 1] & 15) << 2 | arr[i + 2] >>> 6 : 64],
-          tbl[i + 2 < len ? (arr[i + 2] & 63) : 64]
-        );
-      }
-      return str;
-    };
-  }()),
-  decode: (function(i, tbl) {
-    for (i = 0, tbl = {
-        61: 64,
-        47: 63,
-        43: 62
-      }; i < 62; i++) {
-      tbl[i < 26 ? i + 65 : (i < 52 ? i + 71 : i - 4)] = i;
-    } //A-Za-z0-9+/=
-    return function(str) {
-      var j, len, arr, buf;
-      if (!str || !str.length) {
-        return [];
-      }
-      for (i = 0, len = str.length, arr = [], buf = []; i < len; i += 4) { //6,2+4,4+2,6
-        for (j = 0; j < 4; j++) {
-          buf[j] = tbl[str.charCodeAt(i + j) || 0];
-        }
-        arr.push(
-          buf[0] << 2 | (buf[1] & 63) >>> 4,
-          (buf[1] & 15) << 4 | (buf[2] & 63) >>> 2,
-          (buf[2] & 3) << 6 | buf[3] & 63
-        );
-      }
-      if (buf[3] === 64) {
-        arr.pop();
-        if (buf[2] === 64) {
-          arr.pop();
-        }
-      }
-      return arr;
-    };
-  }())
-};
+function encodeBase64(bytes) {
+  const table = [];
+  for (let i = 65; i < 91; i++) table.push(String.fromCharCode(i))
+  for (let i = 97; i < 123; i++) table.push(String.fromCharCode(i))
+  for (let i = 0; i < 10; i++) table.push(i.toString(10))
+  table.push("+");
+  table.push("/");
+
+  let base64 = "";
+  let i = 0;
+  const len = bytes.byteLength;
+  for (i = 0; i < len; i += 3) {
+    if (len === i + 1) { // last 1 byte
+      const a = (bytes[i] & 0xfc) >> 2;
+      const b = ((bytes[i] & 0x03) << 4);
+      base64 += table[a];
+      base64 += table[b];
+      base64 += "==";
+    } else if (len === i + 2) { // last 2 bytes
+      const a = (bytes[i] & 0xfc) >> 2;
+      const b = ((bytes[i] & 0x03) << 4) | ((bytes[i + 1] & 0xf0) >> 4);
+      const c = ((bytes[i + 1] & 0x0f) << 2);
+      base64 += table[a];
+      base64 += table[b];
+      base64 += table[c];
+      base64 += "=";
+    } else {
+      const a = (bytes[i] & 0xfc) >> 2;
+      const b = ((bytes[i] & 0x03) << 4) | ((bytes[i + 1] & 0xf0) >> 4);
+      const c = ((bytes[i + 1] & 0x0f) << 2) | ((bytes[i + 2] & 0xc0) >> 6);
+      const d = bytes[i + 2] & 0x3f;
+      base64 += table[a];
+      base64 += table[b];
+      base64 += table[c];
+      base64 += table[d];
+    }
+  }
+  return base64;
+}
